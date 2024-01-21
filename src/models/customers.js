a780db3e5c1eb15829c1860a10eeaa252ca60f03
Jared-Js <jaredg@MacBook-Pro-de-Jared.local>
2024-01-21 13:27:11 -0600

    Added model of customers

--- src/models/customers.js
+++ src/models/customers.js
@@ -1,1 +1,35 @@
-const { DATATYPES } = require('sequalize')
+const { DataTypes } = require('sequalize');
+const sequalize = require('../config/data');
+const bcrypt = require('bcrypt');
+
+const custommers = sequalize.define('customers', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    }, 
+    nane:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false 
+    },
+    code: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
