134ba1d7b59973bd19b32d72322a35eb39b6f06f
Jared-Js <jaredg@MacBook-Pro-de-Jared.local>
2024-01-21 15:34:11 -0600

    Added model of status

---
+++ src/models/status.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../config/data')
+
+const status = sequelize.define('status', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },  
+    order: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate : {
+            isInt:true,
+            min: 1
+            }
+        }
+    }, {
+        tableName:"status",
+        timestamps: true // Habilita los campos createdAt y updatedAt
+})
