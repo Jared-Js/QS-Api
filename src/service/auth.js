08e6ac5f9c5a8ec80333a0b4b2d8a3572a9a02cd
Jared-Js <jaredg@MacBook-Pro-de-Jared.local>
2024-01-21 18:20:07 -0600

    Added authomaticathed

---
+++ src/service/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const ensureAuth  = (req, res, next) => {
+    if(!req.headers.authorization){
+        return res.status(403).send({message:`without "Authorization"`})
+    } else {
+        try{
+            let token = req.headers.authorization.replace(/Bearer\s+/i,'')
+            var payload = jwt.decode(token,`${process.env.SECRET_KEY}`)
+            if(Math.floor(Date.now() / 1000) >= payload.exp){
+                return res.status(401).send({message: 'Expired token'})
+            }
+        }catch(error){
+            console.error(error);
+            return res.status(401).send({message: "Invalid token!"})
+        }
+        req.user = payload;
+        next()
+    }
+}
+
+module.exports = ensureAuth
