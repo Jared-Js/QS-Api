d9d625a32709d27834df1f5c8310191f774cd37f
Jared <68272012+jgarciax@users.noreply.github.com>
2024-01-23 21:01:39 -0600

    Update customers.js
    "Changes of the config router"

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -8,7 +8,7 @@ const CustomerController = require('../controllers/customers')
 
         router.post('/login', CustomerController.login)
         router.post('/register', CustomerController.register)
-        router.get('/getInfo', CustomerController.getYourInfo)
+        router.get('/getInfo/:id', CustomerController.getYourInfo)
         router.get('/getPackages', ensureAuth, CustomerController.getYourPackages)
         router.put('/updatePassword/:id', ensureAuth, CustomerController.updatePassword)
         router.put('/updateProfile', ensureAuth,CustomerController.editYourAccount)
