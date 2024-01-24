5edbd9520de5f3dfd7a4dbf08fba13c9fc2fdad2
Jared <68272012+jgarciax@users.noreply.github.com>
2024-01-23 21:06:12 -0600

    Update data.js
    "Changes to db in the config"

--- src/config/data.js
+++ src/config/data.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequalize = new Sequelize ({
     database: "postgres",
-    username: "jaredg",
+    username: "postgres",
     password: "1234",
     host: "127.0.0.1",
     dialect: "postgres"
