57cad049bd4c2c2d1cdced1e45767a8398dc23f8
Jared <68272012+jgarciax@users.noreply.github.com>
2024-02-08 08:49:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ require('dotenv').config()
 const app = require('./src/config/app')
 const sequelize = require('./src/config/data')
 
-
-    sequelize.authenticate()
-    .then(() => {
-        console.log('connection to the database successful!');
-        app.initServer();
-    })
-    .catch((error)=> {
-        console.error('Could not connect to the database', error);
-        process.exit(1);
-    })
+sequelize.authenticate()
+.then(() => {
+    console.log('connection to the database successful!');
+    const server = app.initServer();
+    console.log(`Server is running on http://localhost:${server.address().port}`);
+})
+.catch((error)=> {
+    console.error('Could not connect to the database', error);
+    process.exit(1);
+});
